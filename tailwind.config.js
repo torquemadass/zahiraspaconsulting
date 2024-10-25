30d322046a6079461a018479a3a098acd6a8b4fd
torquemadass <ghaniyo43@gmail.com>
2024-10-25 16:49:49 +0700

    feat: add hero title, favicon on header + hero background

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          'sf-pro': ['"SF Pro Display"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
